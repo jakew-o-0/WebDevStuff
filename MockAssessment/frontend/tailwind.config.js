a6460cd32da25cf49e660b17f1165db199ed434e
jakew-o-0 <jakewbailey007@gmail.com>
2023-10-31 16:18:08 +0000

    css no work

--- MockAssessment/frontend/tailwind.config.js
+++ MockAssessment/frontend/tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      'CustomBlue': '#4B00FF',
+      'CustomPurple': '#814DFF',
+      'CustomGreen': '#67F2C4',
+      'CustomRed': '#FASD5D',
+      'CustomBlack': '#333333',
+      'CustomWhite': '#F5F5F5',
+    },
     extend: {},
   },
   plugins: [],
